1e9e3cf6ec290486206963ca448594272c32ab80
koakovski <hkoakovskip@gmail.com>
2025-02-01 12:07:47 -0300

    fix: fix error with filtered fetch

--- lib/components/campus_filter/campust_filter.dart
+++ lib/components/campus_filter/campust_filter.dart
@@ -2,46 +2,24 @@ import 'package:flutter/material.dart';
 import 'package:if_noticies/components/campus_filter/campus_filter_card.dart';
 import 'package:if_noticies/components/campus_filter/campust_filter_item.dart';
 
-typedef OnCampustFilterChange = void Function(List<String>);
+typedef OnCampustFilterChange = void Function();
 
 class CampusFilter extends StatefulWidget {
+  final List<CampusFilterItem> _campusList;
   final OnCampustFilterChange _onCampustFilterChange;
 
-  const CampusFilter({super.key, onCampustFilterChange})
-      : _onCampustFilterChange = onCampustFilterChange;
+  const CampusFilter({
+    super.key,
+    required List<CampusFilterItem> campusList,
+    required OnCampustFilterChange onCampustFilterChange,
+  })  : _campusList = campusList,
+        _onCampustFilterChange = onCampustFilterChange;
 
   @override
   State<CampusFilter> createState() => _CampusFilterState();
 }
 
 class _CampusFilterState extends State<CampusFilter> {
-  final List<CampusFilterItem> _campusList = [
-    CampusFilterItem(name: 'Morrinhos'),
-    CampusFilterItem(name: 'Reitoria'),
-    CampusFilterItem(name: 'Urutaí'),
-    CampusFilterItem(name: 'Campos Belos'),
-    CampusFilterItem(name: 'Catalão'),
-    CampusFilterItem(name: 'Ceres'),
-    CampusFilterItem(name: 'Cristalina'),
-    CampusFilterItem(name: 'Hidrolândia'),
-    CampusFilterItem(name: 'Ipameri'),
-    CampusFilterItem(name: 'Iporá'),
-    CampusFilterItem(name: 'Posse'),
-    CampusFilterItem(name: 'Rio Verde'),
-    CampusFilterItem(name: 'Trindade'),
-    CampusFilterItem(name: 'Polo de Inovação'),
-    CampusFilterItem(name: 'Centro de Referência'),
-  ];
-
-  void onItemSelectChange() {
-    List<String> selectedCampus = _campusList
-        .where((campus) => campus.selected)
-        .map((campus) => campus.name)
-        .toList();
-
-    widget._onCampustFilterChange(selectedCampus);
-  }
-
   @override
   Widget build(BuildContext context) {
     return SizedBox(
@@ -50,12 +28,14 @@ class _CampusFilterState extends State<CampusFilter> {
           margin: const EdgeInsets.symmetric(horizontal: 10, vertical: 4),
           child: ListView.builder(
               scrollDirection: Axis.horizontal,
-              itemCount: _campusList.length,
+              itemCount: widget._campusList.length,
               itemBuilder: (context, position) {
-                CampusFilterItem campus = _campusList[position];
+                CampusFilterItem campus = widget._campusList[position];
                 return CampusFilterCard(
                   campus: campus,
-                  onItemSelectChange: onItemSelectChange,
+                  onItemSelectChange: () {
+                    widget._onCampustFilterChange();
+                  },
                 );
               }),
         ));
--- lib/screens/notices_list_screen.dart
+++ lib/screens/notices_list_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:if_noticies/components/campus_filter/campust_filter.dart';
+import 'package:if_noticies/components/campus_filter/campust_filter_item.dart';
 import 'package:if_noticies/components/notice_card/notice_card.dart';
 import 'package:if_noticies/components/padding_loading_indicator.dart';
 import 'package:if_noticies/entities/notice.dart';
@@ -15,6 +16,23 @@ class NoticesListScreen extends StatefulWidget {
 class _NoticesListScreenState extends State<NoticesListScreen> {
   final IFNoticeApiService iFNoticeApiService = IFNoticeApiService();
   final ScrollController _scrollController = ScrollController();
+  final List<CampusFilterItem> campusList = [
+    CampusFilterItem(name: 'Morrinhos'),
+    CampusFilterItem(name: 'Reitoria'),
+    CampusFilterItem(name: 'Urutaí'),
+    CampusFilterItem(name: 'Campos Belos'),
+    CampusFilterItem(name: 'Catalão'),
+    CampusFilterItem(name: 'Ceres'),
+    CampusFilterItem(name: 'Cristalina'),
+    CampusFilterItem(name: 'Hidrolândia'),
+    CampusFilterItem(name: 'Ipameri'),
+    CampusFilterItem(name: 'Iporá'),
+    CampusFilterItem(name: 'Posse'),
+    CampusFilterItem(name: 'Rio Verde'),
+    CampusFilterItem(name: 'Trindade'),
+    CampusFilterItem(name: 'Polo de Inovação'),
+    CampusFilterItem(name: 'Centro de Referência'),
+  ];
   List<Notice> noticies = [];
   bool isLoading = false;
   bool hasMore = true;
@@ -56,11 +74,26 @@ class _NoticesListScreenState extends State<NoticesListScreen> {
     }
   }
 
-  Future<void> onCampustFilterChange(List<String> campusSelected) async {
+  int? lastNoticeId() {
+    return noticies.isNotEmpty ? noticies.last.idSite : null;
+  }
+
+  List<CampusFilterItem> campustSelected() {
+    return campusList.where((campus) => campus.selected).toList();
+  }
+
+  List<String> campustSelectedNames() {
+    return campustSelected().map((campus) => campus.name).toList();
+  }
+
+  void onCampustFilterChange() {
     setState(() {
       noticies = [];
     });
-    fetchNoticies(campusSelected: campusSelected);
+    fetchNoticies(
+      lastId: lastNoticeId(),
+      campusSelected: campustSelectedNames(),
+    );
   }
 
   void _onScroll() {
@@ -68,7 +101,10 @@ class _NoticesListScreenState extends State<NoticesListScreen> {
         _scrollController.position.maxScrollExtent - 100;
 
     if (isNextToBottom && hasMore) {
-      fetchNoticies(lastId: noticies.isNotEmpty ? noticies.last.idSite : null);
+      fetchNoticies(
+        lastId: lastNoticeId(),
+        campusSelected: campustSelectedNames(),
+      );
     }
   }
 
@@ -87,6 +123,7 @@ class _NoticesListScreenState extends State<NoticesListScreen> {
       body: Column(
         children: [
           CampusFilter(
+            campusList: campusList,
             onCampustFilterChange: onCampustFilterChange,
           ),
           Expanded(
